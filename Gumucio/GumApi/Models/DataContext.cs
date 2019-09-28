dcb91f0390283e05d37f010954ec1eab94d261bc
visual_community@outlook.com <visual_community@outlook.com>
2019-09-28 09:39:41 -0400

    DAtaconxtex

--- Gumucio/GumApi/Models/DataContext.cs
+++ Gumucio/GumApi/Models/DataContext.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace GumApi.Models
+﻿namespace GumApi.Models
 {
     public class DataContext
     {
+
     }
 }
