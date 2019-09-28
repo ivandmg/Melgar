7c1a3c8be5bd149c9590b07ab08de9133c72bc46
visual_community@outlook.com <visual_community@outlook.com>
2019-09-28 10:03:01 -0400

    finished

---
+++ Gumucio/Gumucio/Models/DbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace GumApi.Models
+{
+    public class DbContext
+    {
+        private string v;
+
+        public DbContext(string v)
+        {
+            this.v = v;
+        }
+    }
+}
